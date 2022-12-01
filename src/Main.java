f4660bc0a5ada968995452462b7e3b3f6c7ae8a7
alensden <alensden@gmail.com>
2022-12-01 13:07:13 -0400

    sp accessed

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         CustomerData customerData = new CustomerData();
         customerData.dataBaseConnection();
-        System.out.println("Hello world!");
     }
 }
